4807516a11d9185ed5a4defa8ffa06d653a9f9db
Hamud <hamudalwi91@gmail.com>
2024-03-20 11:48:05 -0700

    9-multiply_by_2.py

---
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+# Hamud001
+
+
+def multiply_by_2(a_dictionary):
+    """a function return a new dictionary with all values multipled by 2."""
+    return {key: value * 2 for key, value in a_dictionary.items()}
