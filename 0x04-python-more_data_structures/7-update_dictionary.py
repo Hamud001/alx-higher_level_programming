0794e5b63a6edb78597fd1cb6a8caaf4a966b607
Hamud <hamudalwi91@gmail.com>
2024-03-20 11:38:47 -0700

    7-update_dictionary.py

---
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+# Hamud001
+
+
+def update_dictionary(a_dictionary, key, value):
+    """function that replace or add key/value in a dictionary."""
+    a_dictionary[key] = value
+    return (a_dictionary)
