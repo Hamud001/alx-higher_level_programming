aaf392d690197d40c938f71b5f3d3f60ef789c8b
Hamud <hamudalwi91@gmail.com>
2024-03-21 03:49:01 -0700

    12-roman_to_int.py

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -17,7 +17,7 @@ def roman_to_int(roman_string):
             "D": 500,
             "M": 1000
     }
-    n = 0
+    num = 0
 
     for i in range(len(roman_string)):
         if roman_dict.get(roman_string[i], 0) == 0:
@@ -25,8 +25,8 @@ def roman_to_int(roman_string):
 
         if (i != (len(roman_string) - 1) and
                 roman_dict[roman_string[i]] < roman_dict[roman_string[i + 1]]):
-                n += roman_dict[roman_string[i]] * -1
+                num += roman_dict[roman_string[i]] * -1
 
         else:
-            n += roman_dict[roman_string[i]]
-    return (n)
+            num += roman_dict[roman_string[i]]
+    return (num)
