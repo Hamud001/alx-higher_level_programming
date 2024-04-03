846744768b8930f87c535783c02ebfaaa5a061e3
Hamud <hamudalwi91@gmail.com>
2024-04-02 20:06:39 -0700

    1-safe_print_integer.py

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -2,6 +2,7 @@
 def safe_print_integer(value):
     try:
         print("{:d}".format(value))
-        return True
-    except:
+    except Exception as exe:
         return False
+    else:
+        return True
